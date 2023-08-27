74287acb276089f8d64c02e9e1bd496de9019541
VickyL86 <lasotaludwika1@gmail.com>
2023-08-27 16:03:47 -0500

    Add national revenue data graph to home

--- HTML/test.js
+++ HTML/test.js
@@ -87,148 +87,46 @@ fetch(link)
         });
     });
 
-//____________________Graps_______________________________________________________
+//____________________Graps_____VICKY__________________________________________________
 
+// fetch data from json "../json/national_market.json" and prepare a line graph with sum of revenue for each year
 d3.json("../json/national_market.json").then(function(data) {
-    // Convert the data into an array of objects
-    const dataArray = Object.values(data);
+    console.log(data);
 
-    // Parse dates and remove dollar signs and commas
-    const parseTime = d3.timeParse("%Y-%m-%d");
+    // Convert object to an array of values
+    var dataArray = Object.values(data);
+
+    // Prepare data for summing revenue by year
+    var revenueByYear = {};
+
+    // Loop through the data and sum revenue by year
     dataArray.forEach(function(d) {
-        d.month = parseTime(d.month);
-        d.revenue = parseFloat(d.revenue.replace(/[\$,]/g, ''));
-        d.taxes = parseFloat(d.taxes.replace(/[\$,]/g, ''));
+        var year = new Date(d.month).getFullYear();
+        var revenue = parseFloat(d.revenue.replace(/[\$,]/g, '')); // Convert revenue to a number
+        if (!revenueByYear[year]) {
+            revenueByYear[year] = revenue;
+        } else {
+            revenueByYear[year] += revenue;
+        }
     });
 
-    // Group data by year and calculate sums
-    const dataByYear = d3.nest()
-        .key(function(d) { return d.month.getFullYear(); })
-        .rollup(function(v) { return {
-            revenue: d3.sum(v, function(d) { return d.revenue; }),
-            taxes: d3.sum(v, function(d) { return d.taxes; })
-        }; })
-        .entries(dataArray);
+    // Extract years and revenue for plotting
+    var years = Object.keys(revenueByYear);
+    var revenue = Object.values(revenueByYear);
 
-    // Set up dimensions for the graph
-    const margin = {top: 20, right: 30, bottom: 30, left: 40};
-    const width = 800 - margin.left - margin.right;
-    const height = 400 - margin.top - margin.bottom;
+    // Create a line graph
+    var trace1 = {
+        x: years,
+        y: revenue,
+        type: "line"
+    };
 
-    // Create SVG element
-    const svg = d3.select("#line-graph")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var data = [trace1];
+    var layout = {
+        title: "Total Revenue from Sports Betting in the United States by Year",
+        xaxis: { title: "Year" },
+        yaxis: { title: "Total Revenue (in millions)" }
+    };
 
-    // Define scales for x and y axes
-    const x = d3.scaleTime()
-        .domain(d3.extent(dataArray, function(d) { return d.month; }))
-        .range([0, width]);
-
-    const y = d3.scaleLinear()
-        .domain([0, d3.max(dataByYear, function(d) { return Math.max(d.value.revenue, d.value.taxes); })])
-        .nice()
-        .range([height, 0]);
-
-    // Define line functions
-    const lineRevenue = d3.line()
-        .x(function(d) { return x(d.key); })
-        .y(function(d) { return y(d.value.revenue); });
-
-    const lineTaxes = d3.line()
-        .x(function(d) { return x(d.key); })
-        .y(function(d) { return y(d.value.taxes); });
-
-    // Append x and y axes
-    svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
-
-    svg.append("g")
-        .call(d3.axisLeft(y));
-
-    // Append revenue line
-    svg.append("path")
-        .data([dataByYear])
-        .attr("class", "line")
-        .style("stroke", "blue")
-        .attr("d", lineRevenue);
-
-    // Append taxes line
-    svg.append("path")
-        .data([dataByYear])
-        .attr("class", "line")
-        .style("stroke", "green")
-        .attr("d", lineTaxes);
+    Plotly.newPlot("line-graph", data, layout); // Make sure to target the correct element ID here
 });
-
-
-
-
-//______________________________________VICKY________________________________________________________________________________________
-
-// Function to fetch and display data for the selected JSON file
-function fetchAndDisplayData(jsonUrl) {
-    // Fetch JSON data
-    fetch(jsonUrl)
-        .then(response => response.json())
-        .then(data => {
-            // Clear existing markers on the map
-            map.eachLayer(function (layer) {
-                if (layer instanceof L.Marker) {
-                    map.removeLayer(layer);
-                }
-            });
-
-            // Add markers to the map based on data (example)
-            if (data.markers) {
-                data.markers.forEach(function (markerData) {
-                    L.marker([markerData.lat, markerData.lon]).addTo(map);
-                });
-            }
-
-            // Display charts based on data using Chart.js
-            if (data.barChart) {
-                displayBarChart(data.barChart.sampleValues, data.barChart.otuIds, data.barChart.otuLabels);
-            }
-            if (data.bubbleChart) {
-                displayBubbleChart(data.bubbleChart.sampleValues, data.bubbleChart.otuIds, data.bubbleChart.otuLabels);
-            }
-            if (data.gaugeChart) {
-                displayGaugeChart(data.gaugeChart.washFreq);
-            }
-
-        })
-        .catch(error => console.error("Error fetching JSON data:", error));
-}
-
-// Function to display bar chart based on data using Chart.js
-function displayBarChart(sampleValues, otuIds, otuLabels) {
-    // Chart.js bar chart code here
-}
-
-// Function to display bubble chart based on data using Chart.js
-function displayBubbleChart(sampleValues, otuIds, otuLabels) {
-    // Chart.js bubble chart code here
-}
-
-// Function to display gauge chart based on data using Chart.js
-function displayGaugeChart(washFreq) {
-    // Chart.js gauge chart code here
-}
-
-// Function to handle JSON file change
-function jsonChange() {
-    const selectedJsonUrl = dropdown.property("value");
-    fetchAndDisplayData(selectedJsonUrl);
-}
-
-// Set up event listener for JSON file changes
-dropdown.on("change", jsonChange);
-
-// Initialize page with data from the first JSON file
-const initialJsonUrl = jsonFiles[0].url;
-fetchAndDisplayData(initialJsonUrl);
